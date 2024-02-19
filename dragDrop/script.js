4804b2c6236a14145c71f5b4d4930b8eef91d3e0
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-02-19 18:08:25 +0100

    19/02

---
+++ dragDrop/script.js
@@ -0,0 +1,6 @@
+let arrayFitxers=[];
+const dropArea = document.querySelector('.dropArea');
+const dragDropText = document.querySelector('h2');
+const button = document.querySelector('button');
+const input = document.querySelector('#input-file');
+const preview = document.querySelector('#preview');
