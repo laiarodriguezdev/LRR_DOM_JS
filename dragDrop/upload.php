e578e53ebd3ce72078cbf546f10d9a706dce984e
Laia <laia.rodriguez@enti.cat>
2024-02-22 21:24:23 +0100

    Ara si que php inclos

---
+++ dragDrop/upload.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_FILES["inputFiles"])) {
+    print_r($_FILES["inputFiles"]);
+    for ($i = 0; $i < count($_FILES["inputFiles"]["name"]); $i++) {
+        echo $_FILES["inputFiles"]["tmp_name"][$i] . '<br>';
+        echo $_FILES["inputFiles"]["name"][$i] . '<br>';
+    
+    }
+} else {
+    echo "No s'han rebut documents";
+}
+?>
