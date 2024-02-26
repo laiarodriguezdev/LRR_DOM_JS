f05b82c7db30f9e03108a2fd38a5eab5014e2b65
LaiaRodriguez22 <lrodrigu22@jaumebalmes.net>
2024-02-26 15:48:11 +0100

    Contrasenyes comprovació

--- formulari/script.js
+++ formulari/script.js
@@ -21,6 +21,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+/*--CORREU--*/
+
 function validateEmail(email) {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
     return true;
@@ -34,14 +36,73 @@ document.addEventListener("DOMContentLoaded", function () {
     const email = emailInput.value.trim();
     if (validateEmail(email)) {
       emailInput.style.borderColor = "green";
-      // Limpiar mensaje de error
       document.getElementById("emailError").innerText = "";
     } else {
       emailInput.style.borderColor = "red";
-      // Mostrar mensaje de error
       document.getElementById("emailError").innerText =
         "El correu electronic no és valid";
       document.getElementById("emailError").style.color = "red";
+    }
+  });
+});
+
+/*--CONTRASENYA 1--*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  contrasenyaInput.addEventListener("input", function () {
+    const password = this.value.trim();
+    let errors = [];
+
+    if (password.length < 8 || password.length > 15) {
+      errors.push("La contrasenya ha de tenir entre vuit i quinze lletres.");
+    }
+
+    if (!/[a-z]/.test(password)) {
+      errors.push("La contrasenya ha de tenir una lletra minuscula");
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      errors.push("La contrasenya ha de tenir una lletra majuscula");
+    }
+
+    if (!/[0-9]/.test(password)) {
+      errors.push("La contrasenya ha de tenir un dígit numeric.");
+    }
+
+    if (!/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password)) {
+      errors.push("La contrasenya ha de tenir un caracter especial.");
+    }
+
+    mostrarErrores(contrasenyaInput, errors);
+  });
+
+  function mostrarErrores(input, errors) {
+    if (errors.length > 0) {
+      input.style.borderColor = "red";
+      document.getElementById("contraError").innerText = errors.join(" \n ");
+      document.getElementById("contraError").style.color = "red";
+    } else {
+      input.style.borderColor = "green";
+      document.getElementById("contraError").innerText = "";
+    }
+  }
+});
+
+/*--CONTRASENYA 2--*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  contrasenya2Input.addEventListener("focusout", function () {
+    const password = contrasenyaInput.value.trim();
+    const confirmPassword = contrasenya2Input.value.trim();
+
+    if (password === confirmPassword && confirmPassword !== "") {
+      contrasenya2Input.style.borderColor = "green";
+      document.getElementById("contra2Error").innerText = "";
+    } else {
+      contrasenya2Input.style.borderColor = "red";
+      document.getElementById("contra2Error").innerText =
+        "Les contrasenyes no coincideixen";
+      document.getElementById("contra2Error").style.color = "red";
     }
   });
 });
