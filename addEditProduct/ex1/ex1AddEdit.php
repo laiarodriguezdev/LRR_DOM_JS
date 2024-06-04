eb4930a74468f46e9cb3e7d5be11c174c38f6583
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-06-04 20:19:17 +0200

    ex1 fet

--- addEditProduct/ex1/ex1AddEdit.php
+++ addEditProduct/ex1/ex1AddEdit.php
@@ -1,35 +1,38 @@
-<?
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "lrraddedit";
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lrraddedit";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST["nomProducte"]) && !empty($_POST["nomProducte"])) {
+    $nomProducte = $conn->real_escape_string($_POST["nomProducte"]);
+    $addEdit = intval($_POST["addEdit"]);
+    
+    if ($addEdit == 0) {
+        $sql = "INSERT INTO productes (nomProducte) VALUES ('$nomProducte')";
+    } else {
+        $sql = "UPDATE productes SET nomProducte='$nomProducte' WHERE id=$addEdit";
     }
-        
-    if(isset($_POST["nomProducte"]) && !empty($_POST["nomProducte"])){
-        if($_POST["addEdit"]==0){
-            $sql = "INSERT INTO productes (nomProducte) VALUES ('" . $_POST["nomProducte"] ."')";
-        }else{
-            $sql = "UPDATE productes SET nomProducte='" . $_POST["nomProducte"] . "' WHERE id=" . $_POST["addEdit"];
-        }
-        
 
-        echo $sql;
+    echo $sql;
 
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        $conn->close();
-
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
+
+    $conn->close();
     
     header('Location: ex1Llistat.php');
-
+    exit();  
+} else {
+    echo "No product name provided";
+}
 ?>
