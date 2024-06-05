c1d5503de9e9dc1ea465302ce5a3f4f2d940e62e
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-06-05 17:41:20 +0200

    fent el fetch que no se pq peta

--- fetchSelects/script.js
+++ fetchSelects/script.js
@@ -1,47 +1,47 @@
 const categoriaSelect = document.getElementById("categoriaSelect");
 const subcategoriaSelect = document.getElementById("subcategoriaSelect");
 
-let formData = new FormData();
-
-let options = {
-  method: 'POST',
-  body: formData
-}
-
-//CATEGORIA
+// CATEGORIA
 fetch("getCatGets.php")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((categoria) => {
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(categoria => {
       let optionSelect = document.createElement("option");
       optionSelect.value = categoria.id; 
       optionSelect.text = categoria.name; 
       categoriaSelect.appendChild(optionSelect);
     });
   })
-  .catch((error) => {
-    console.error("Error", error);
+  .catch(error => {
+    console.error("Error:", error);
   });
 
-  categoriaSelect.addEventListener("change", function() {
-    let categoriaValor = this.value;
-    console.log("Valor:", categoriaValor);
+categoriaSelect.addEventListener("change", function() {
+  let categoriaValor = this.value;
+  console.log("Valor:", categoriaValor);
 
-    formData.append("categoria", valor); 
-    subcategoriaSelect.innerHTML = '';
+  let formData = new FormData();
+  formData.append("categoria", categoriaValor); 
 
-//SUBCATEGORIA
-fetch("getSubGets.php", options)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((subcategoria) => {
+  subcategoriaSelect.innerHTML = '';
+
+  let options = {
+    method: 'POST',
+    body: formData
+  };
+
+  // SUBCATEGORIA
+  fetch("getSubGets.php", options)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(subcategoria => {
         let optionSelect = document.createElement("option");
         optionSelect.value = subcategoria.id;
         optionSelect.text = subcategoria.name;
         subcategoriaSelect.appendChild(optionSelect);
       });
     })
-    .catch((error) => {
-      console.error("Error: ", error);
+    .catch(error => {
+      console.error("Error:", error);
     });
 });
